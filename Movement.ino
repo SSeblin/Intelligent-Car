4d94346b2b8661b8ad70393aee4a170803f3ac1a
Sebastien Seblin <sseblin@appstud.me>
2017-06-18 16:33:29 +0200

    Add files via upload

--- Movement.ino
+++ Movement.ino
@@ -1,5 +1,5 @@
 /*******************************INTELLIGENT CAR ************************************
-*  EQUIPMENT: ARDUINO + L293D
+*  EQUIPMENT: ARDUINO + L293D + Ulltra Sonic Sensor 
 *  Author：Sebastien Seblin       
 *  Licence: Open Source
 ******************************************************************************************/
@@ -19,6 +19,19 @@ int input2=4;
 //Input Pins Motor 2
 int input3=5;
 int input4=6;
+
+/******************************************************************************************
+Ultrasonic Sensor Pins Declaration
+******************************************************************************************/
+int const trigPin = 10;
+int const echoPin = 9;
+
+/******************************************************************************************
+Security Distance
+******************************************************************************************/
+ 
+int securityDistance=10; 
+
 
 /******************************************************************************************
 Move Forward function
@@ -113,14 +126,20 @@ void carStop()
 ******************************************************************************************/
 void setup()
 {
-      pinMode(2, OUTPUT);       // Define the number 14 interface as the output interface
-      pinMode(3, OUTPUT);       // Define the number 15 interface as the output interface
-      pinMode(4, OUTPUT);       // Define the number 16 interface as the output interface
-      pinMode(5, OUTPUT);       // Define the number 16 interface as the output interface
-      pinMode(6, OUTPUT);       // Define the number 16 interface as the output interface
-      pinMode(7, OUTPUT);       // Define the number 16 interface as the output interface
-      
-      
+      //L293D PinMode
+      pinMode(2, OUTPUT);       // Define the number 2 interface as the output interface
+      pinMode(3, OUTPUT);       // Define the number 3 interface as the output interface
+      pinMode(4, OUTPUT);       // Define the number 4 interface as the output interface
+      pinMode(5, OUTPUT);       // Define the number 5 interface as the output interface
+      pinMode(6, OUTPUT);       // Define the number 6 interface as the output interface
+      pinMode(7, OUTPUT);       // Define the number 7 interface as the output interface
+
+      //Ultra Sonic PinMode
+      pinMode(trigPin, OUTPUT); // trig pin will have pulses output
+      pinMode(echoPin, INPUT); // echo pin should be input to get pulse width
+
+      //Serial Port Initialization
+      Serial.begin(9600);
 }
 
 /******************************************************************************************
@@ -129,15 +148,46 @@ Main Program
 void loop()
 {
 
-//Tests 
 
-moveForward();
-//moveBackward();
-//moveRight();
-//moveLeft();
-//carStop();
-     
+/******************************************************************************************
+Ultra Sonic Sensor Distance Calculation
+******************************************************************************************/ 
+
+ 
+int duration, distance; // Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
+
+digitalWrite(trigPin, HIGH); // Output pulse with 1ms width on trigPin
+delay(1);
+digitalWrite(trigPin, LOW);
+
+duration = pulseIn(echoPin, HIGH); // Measure the pulse input in echo pin
+// Distance is half the duration devided by 29.1 (from datasheet)
+
+distance = (duration/2) / 29.1;
+delay(60);
+
+
+/******************************************************************************************
+Movement Loop
+******************************************************************************************/ 
+
+/*Tests on the Serial Port if needed
+Serial.print("Distance: "); 
+Serial.println(distance );
+*/
+    moveForward(); // Normal condition
+
+if (distance < securityDistance){ // If obstacle is close, move right to avoid it
+  moveRight(); 
 }
+
+if (distance < securityDistance/2){ // If obstacle is too close, Stop 
+  carStop(); 
+}
+
+  }
+    
+
 /******************************************************************************************
 *                                       END
 ******************************************************************************************/
